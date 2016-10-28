65847431dd46d8ad5225d77f6706ffedc60f4392
gvlcek <gvlcek@icloud.com>
2016-10-28 10:47:38 -0300

    first commit

---
+++ BlueTrailTest/WeatherData.swift
@@ -0,0 +1,57 @@
+//
+//  WeatherData.swift
+//  BlueTrailTest
+//
+//  Created by Guadalupe Vlcek on 27/10/16.
+//  Copyright © 2016 Vlcek Guadalupe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+class weatherForecast {
+    let temperature: Double
+    let date: Date
+    let min: Double
+    let max: Double
+    let state: String
+    let iconState: String
+    
+    lazy var icon: UIImage = { return UIImage(named: self.iconState)! }()
+    
+    init(temperature: Double, date: Date, min: Double, max: Double, state: String, iconState: String) {
+        self.temperature = temperature
+        self.date        = date
+        self.min         = min
+        self.max         = max
+        self.state       = state
+        self.iconState   = iconState
+    }
+}
+
+class obtainForecast {
+    
+    func callAlamo(url: String){
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+        })
+    }
+    
+    func parseData(JSONData: Data) {
+        do {
+            let redeableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! [String : AnyObject]
+            //print(redeableJSON)
+            
+            if let list = redeableJSON["list"] {
+                print(list)
+            }
+            
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-BlueTrailTestTests/Pods-BlueTrailTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlueTrailTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_BlueTrailTestTests
+@end
