84cc60371841c69146738e0aacf43f2bf1a974e7
gvlcek <gvlcek@icloud.com>
2016-11-06 08:17:24 -0300

    works as it should

--- BlueTrailTest/TableViewController.swift
+++ BlueTrailTest/TableViewController.swift
@@ -14,60 +14,14 @@ class TableViewController: UITableViewController {
     
     let apiURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=buenos%20aires&mode=json&units=metric&APPID=3d7fafd6fbae7ba96a7b3fa31bd0ce6b"
     
-    class weatherForecast {
-        let temperature: Double
-        let min: Double
-        let max: Double
-        let state: String
-        
-        init(temperature: Double, min: Double, max: Double, state: String) {
-            self.temperature = temperature
-            self.min         = min
-            self.max         = max
-            self.state       = state
-        }
-    }
-    
-    var days = [weatherForecast]()
-    
     func callAlamo(url: String){
         Alamofire.request(url).responseJSON(completionHandler: {
             response in
-            self.parseData(JSONData: response.data!)
-        })
-    }
-    
-    func parseData(JSONData: Data) {
-        do {
-            let redeableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! [String : AnyObject]
-            
-            if let list = redeableJSON["list"] {
-                
-                for i in 0..<list.count {
-                    let item = list[i] as! [String : AnyObject]
-                    
-                    let temp = item["temp"]
-                    
-                    let day = temp?["day"]
-                    let min = temp?["min"]
-                    let max = temp?["max"]
-                    
-                    if let weathers = item["weather"] {
-                        let weather = weathers[0] as! [String : AnyObject]
-                        let description = weather["description"]
-                        
-                        let w = weatherForecast(temperature: day!! as! Double, min: min!! as! Double, max: max!! as! Double, state: description! as! String)
-                        
-                        days.append(w)
-                        self.tableView.reloadData()
-                    }
-                }
+            parseData(JSONData: response.data!)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
             }
-        }
-        catch{
-            print(error)
-        }
-        
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
