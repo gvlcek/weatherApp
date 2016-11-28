f8d3df2466ae52d5eea8ec449500347517b63859
gvlcek <gvlcek@icloud.com>
2016-11-27 23:17:14 -0300

    finished app

--- BlueTrailTest/AppDelegate.swift
+++ BlueTrailTest/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UNUserNotificationCenter.current().delegate = self
-        //application.setMinimumBackgroundFetchInterval(3600)
         application.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
         return true
     }
